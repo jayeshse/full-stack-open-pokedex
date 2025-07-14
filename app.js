a406cdf814c6a4f73a0b33772edf0b2112ce5f2f
jayeshm <jayesh.mengane99@gmail.com>
2025-07-15 00:38:20 +0530

    new output

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.listen(port,'0.0.0.0', () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('yes bitches') // change this string to ensure a new version deployed
 })
